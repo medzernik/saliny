200857506428191ccce501ae768258854ed1a1d9
medze <medzernik1@gmai.com>
2022-01-06 19:52:43 +0100

    first commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+
+
+struct Linka {
+    cislo: uint,
+    zastavky: Vec<Zastavka>
+}
+
+struct Odchod {
+    cislo: uint,
+    odchod: &str,
+}
+
+struct Zastavka {
+    nazov: &str,
+    harmonogram: Vec<Odchod>,
+    pristup: bool,
+}
+
+
+
 fn main() {
-    println!("Hello, world!");
+
+
+
 }
