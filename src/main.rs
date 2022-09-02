7c1ab200952b0efd650c0587b3b94e5c1a77c56c
medzernik <medzernik1@gmail.com>
2022-09-02 15:50:37 +0200

    starting again

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,3 @@
-
-
-pub struct Linka {
-    cislo: u64,
-    zastavky: Vec<String>
-}
-
-impl Linka {
-    pub fn create(cislo: u64, zastavky: Vec<String>) -> Linka {
-        Linka {
-            cislo: cislo,
-            zastavky: zastavky,
-        }
-
-    }
-}
-
-struct Odchod {
-    cislo: u64,
-    odchod: String,
-}
-
-struct Zastavka {
-    nazov: String,
-    harmonogram: Vec<Odchod>,
-    pristup: bool,
-}
-
-
-
-fn main() {
-    let linka1 = Linka::create(2, vec![String::from("Hlavna stanica"), String::from("Pod stanicou")]);
-    println!("Linka: {}, zastavky: {}", linka1.cislo, linka1.zastavky[0]);
-
+fn main(){
 
 }
